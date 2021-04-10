d3f6e2e28a3fbee4ab15526258debd59a0ebd208
umitao <umitao@gmail.com>
2021-04-10 23:26:44 +0200

    complete with lyrics functionality

---
+++ client/src/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=fa8012eb607e46c2ad5a683cdb9823ad&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+export default function Login() {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-lg btn-success" href={AUTH_URL}>
+        Login with Spotify
+      </a>
+    </Container>
+  );
+}
